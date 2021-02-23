b527c4ead60bd315877c1600768048318e6bddfb
Nikhil R <55541317+NikhilRaghav@users.noreply.github.com>
2021-02-23 14:36:54 +0530

    Create Assignment_1_Ques_1_and_2.py

---
+++ Data Structure and Algorithm with Python Batch 2 - FEB LetsUpgrade Toutube Pgm/Assignments/Assignment_1_Ques_1_and_2.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number"))
+b=int(input("Enter another Number "))
+#Question 1 
+print("Addition of two Numbers is ",a+b)
+print("Subtration of two Numbers is ",a-b)
+print("Multiplication of two Numbers is ",a*b)
+print("Division of two Numbers is ",a/b)
+print("Floor Division of two Numbers is ", a//b)
+#Question 2
+print("a ^b = ",a**b)
